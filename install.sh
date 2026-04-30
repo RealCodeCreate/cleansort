d44c5fc6c9155bf9bfdebeca500247d2c6679d07
RealCodeCreate <kwnh6rfpq6@privaterelay.appleid.com>
2026-05-01 00:18:33 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo curl -L https://githubusercontent.com -o /usr/local/bin/cleansort
+sudo curl -L https://github.com/RealCodeCreate/cleansort/blob/main/cleansort -o /usr/local/bin/cleansort
 sudo chmod +x /usr/local/bin/cleansort
 echo "Установка завершена! Теперь вводи в терминале: cleansort"
